45acdd5a42363d3cc5ddea32807392ef83f0b953
M.RAY <a.mray47@yahoo.fr>
2021-09-28 22:47:22 +0800

    Mounted the style.css

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ app.get("/", (req, res)=> {
 	res.sendFile(__dirname + "/views/index.html");
 })
 
+// mount the full path of the style.css file
+app.use("/public", express.static(__dirname + "/public"));
+// Server the styles.css file
+app.use(express.static(__dirname + "/public/style.css"));
 
 
 
