5980dcbe82314933cae0b44a8adff711f89cf5cf
M.RAY <a.mray47@yahoo.fr>
2021-10-02 02:57:09 +0800

    fixed typo

--- myApp.js
+++ myApp.js
@@ -29,9 +29,10 @@ app.get("/json", (req, res)=> {
 app.get("/now", (req, res, next)=> {
 	req.time = new Date().toString();
 	next();
+},
 	(req, res)=> {
-		res.send({"time": req.time});
-	}
+		res.send({time: req.time});
+	
 })
 
 
